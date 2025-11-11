7ae85f2d26473bcedd4d34ae59058916a8a49225
adalieren <erenada2@gmail.com>
2025-11-11 14:44:35 +0300

    Updade

--- LinkedList/src/main/java/org/example/Main.java
+++ LinkedList/src/main/java/org/example/Main.java
@@ -1,27 +1,22 @@
 package org.example;
 
-import org.example.LinkedList;
-
-public class Main {
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class main {
 
     public static void main(String[] args) {
-
         LinkedList list = new LinkedList();
 
-        list.add("This");
-        list.add("is");
-        list.add("a");
-        list.add("list");
+        list.addToEnd("This");
+        list.addToEnd("is");
+        list.addToEnd("a");
+        list.addToEnd("list");
 
         list.yazdir();
 
+        
+        list.deleteLast();
 
-
-
-
-
-
-
+        list.yazdir();
     }
-
 }
